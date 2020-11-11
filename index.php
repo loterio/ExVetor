f0d70e8af4840135a99b04b5aff3169c4b429c24
fabio loterio <vitorloterio@gmail.com>
2020-11-11 10:02:36 -0300

    development

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP Simple Examples</title>
+</head>
+<body>
+
+    <header class="plx">
+        <h1>So, you're welcome.</h1>
+        <p>there are some examples of php basic syntax</p>
+        <small>( i wish that you'll like it )</small>
+        <!-- <h3>¯\_(ツ)_/¯</h3> -->
+    </header>
+
+    <section>
+        <div class="container">
+            <h1 class="title">Main projects.</h1>
+
+            <ul>
+                <li class="dropdown">
+                    <span>loops</span>
+                    <div class="dropdown-content">
+                        <a href="loops/Exemplo1.php">ex 1</a><br>
+                        <a href="loops/Exemplo2.php">ex 2</a>
+                    </div>
+                </li>
+
+                <li class="dropdown">
+                    <span>forms</span>
+                    <div class="dropdown-content">
+                        <a href="forms/Exemplo1.php">ex 1</a><br>
+                        <a href="forms/Exemplo2.php">ex 2</a><br>
+                        <a href="forms/Exemplo3.php">ex 3</a>
+                    </div>
+                </li>
+
+                <li class="dropdown">
+                    <span>datas</span>
+                    <div class="dropdown-content">
+                        <a href="datas/Exemplo1.php">ex 1</a><br>
+                        <a href="datas/Exemplo2.php">ex 2</a>
+                    </div>
+                </li>
+            </ul>
+
+        </div>
+    </section>
+
+</body>
+</html>
